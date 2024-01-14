6c0a2bd7e29f6e5de0e7b4b2aca0e22a76a033a7
twiby <t.balenbois@gmail.com>
2024-01-14 17:20:21 +0100

    add benchmark for access

---
+++ benches/access.rs
@@ -0,0 +1,58 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::collections::HashMap;
+
+use containers::SparseSet;
+
+pub fn vec_access<const N: usize>(c: &mut Criterion) {
+    let mut name = "vec access ".to_string();
+    name.push_str(&N.to_string());
+
+    let mut vec = Vec::new();
+    for i in 0..N {
+        vec.push(i);
+    }
+    let mid = N >> 1;
+
+    c.bench_function(name.as_str(), |b| b.iter(|| {
+        black_box(vec[black_box(mid)]);
+    }));
+}
+
+pub fn map_access<const N: usize>(c: &mut Criterion) {
+    let mut name = "map access ".to_string();
+    name.push_str(&N.to_string());
+
+    let mut map = HashMap::new();
+    for i in 0..N {
+        map.insert(i, i);
+    }
+    let mid = N >> 1;
+
+    c.bench_function(name.as_str(), |b| b.iter(|| {
+        black_box(map[&black_box(mid)]);
+    }));
+}
+
+pub fn sparseset_access<const N: usize>(c: &mut Criterion) {
+    let mut name = "sparseset access ".to_string();
+    name.push_str(&N.to_string());
+
+    let mut set = SparseSet::new();
+    let mut indices = Vec::new();
+    for i in 0..N {
+        indices.push(set.insert(i));
+    }
+    let mid = N >> 1;
+
+    c.bench_function(name.as_str(), |b| b.iter(|| {
+        black_box(set[black_box(mid)]);
+    }));
+}
+
+criterion_group!(
+    access, 
+    vec_access<10_000>, 
+    map_access<10_000>, 
+    sparseset_access<10_000>
+);
+criterion_main!(access);
